bed6a7a49197aa579568ef3c5cc7acca76e2492b
장태근 <devmeeple@gmail.com>
2025-01-28 01:00:25 +0900

    Feat comment

---
+++ src/main/java/asis/CommentJava.java
@@ -0,0 +1,23 @@
+package asis;
+
+/* IntelliJ, Clean Code
+https://blog.jetbrains.com/idea/2022/04/comment-your-code-like-a-pro-with-intellij-idea/
+
+잘 달린 주석은 그 어떤 정보보다 유용하다. 경솔하고 근거 없는 주석은 코드를 이해하기 어렵게 만든다. 오래되고 조잡한 주석은 거짓과 잘못된 정보를 퍼뜨려 해약을 미친다.
+<출처: [[Clean Code](로버트 C. 마틴, 인사이트, 2013)](https://product.kyobobook.co.kr/detail/S000001032980)>
+
+주석은 컴퓨터가 아닌, 사람에게 의도를 전달한다. 하지만 가급적, 코드로 컴퓨터와 사람에게 의도를 전달하는 방법을 탐구하자. 최후의 선택지로 권장한다.
+ */
+
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // single-line (or end-of-line) hello java1
+//        Single-line comments, command + /
+
+        /* multi-line comments(Block)
+        option + command
+        */
+
+    }
+}
