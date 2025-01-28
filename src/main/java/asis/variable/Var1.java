01d6131d834b06a43b2d3f9ccd8f6c3266223004
장태근 <devmeeple@gmail.com>
2025-01-28 23:15:23 +0900

    Feat hardcoded values 2

--- src/main/java/asis/variable/Var1.java
+++ src/main/java/asis/variable/Var1.java
@@ -3,8 +3,8 @@ package asis.variable;
 public class Var1 {
 
     public static void main(String[] args) {
-        System.out.println(10);
-        System.out.println(10);
-        System.out.println(10);
+        System.out.println(20);
+        System.out.println(20);
+        System.out.println(20);
     }
 }
