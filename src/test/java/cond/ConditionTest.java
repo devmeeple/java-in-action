44534d9d1198fb142cc4e4cbbee465736fb84fd6
장태근 <devmeeple@gmail.com>
2025-01-30 12:17:15 +0900

    Test selectVehicleFor

--- src/test/java/cond/ConditionTest.java
+++ src/test/java/cond/ConditionTest.java
@@ -1,5 +1,6 @@
 package cond;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -31,6 +32,19 @@ class ConditionTest {
         assertEquals("유효한 점수가 아닙니다. 다시 확인 바랍니다", exception.getMessage());
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "1, 도보",
+            "10, 자전거",
+            "100, 자동차",
+            "101, 비행기"
+    })
+    void selectVehicleFor_Distance_ShouldReturnExpectedVehicle(int distance, String expectedVehicle) {
+        String result = selectVehicleFor(distance);
+
+        assertEquals(expectedVehicle, result);
+    }
+
     private String calculateGradeFor(int studentScore) {
         if (studentScore < 0 || studentScore > 100) {
             throw new IllegalArgumentException("유효한 점수가 아닙니다. 다시 확인 바랍니다");
@@ -49,5 +63,20 @@ class ConditionTest {
             return "D";
         }
         return "F";
+    }
+
+    // TODO: 2025.01.30 다형성, 값 Enum, 예외
+    private String selectVehicleFor(int distance) {
+        if (distance <= 1) {
+            return "도보";
+        }
+        if (distance <= 10) {
+            return "자전거";
+        }
+        if (distance <= 100) {
+            return "자동차";
+        }
+
+        return "비행기";
     }
 }
