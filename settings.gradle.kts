2f97f9dea5441b51e3bbdf7b09795c4bca5f8683
devmeeple <devmeeple@gmail.com>
2025-04-03 10:48:08 +0900

    환경설정 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,5 +6,4 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "jvm-in-action"
-
+rootProject.name = "java-in-action"
