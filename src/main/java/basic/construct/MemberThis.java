ac65db828edf56254ad242bf7c0d889bef8a0e44
TaeGeun Jang <devmeeple@gmail.com>
2025-04-08 09:52:04 +0900

    [5-2] this

---
+++ src/main/java/basic/construct/MemberThis.java
@@ -0,0 +1,12 @@
+package basic.construct;
+
+/**
+ * this의 생략
+ */
+public class MemberThis {
+    String nameField;
+
+    void initMember(String nameParameter) {
+        nameField = nameParameter;
+    }
+}
