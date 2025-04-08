cf8d3b51aa689ad7d1759a797acdcbdd54138841
TaeGeun Jang <devmeeple@gmail.com>
2025-04-08 11:03:21 +0900

    [5-5] 생성자 - 오버로딩과 this()

--- src/main/kotlin/basic/construct/KMemberConstruct.kt
+++ src/main/kotlin/basic/construct/KMemberConstruct.kt
@@ -5,6 +5,8 @@ class KMemberConstruct(
     val age: Int,
     val grade: Int,
 ) {
+    constructor(name: String, age: Int) : this(name, age, 50)
+
     init {
         println("생성자 호출 name= ${name}, age= ${age}, grade= ${grade}")
     }
