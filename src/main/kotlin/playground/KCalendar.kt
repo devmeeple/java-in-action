6f3429545972dd5313e54b332d2cba8bbf114bc7
TaeGeun Jang <devmeeple@gmail.com>
2025-04-09 18:16:52 +0900

    [JP1] 문제와 풀이 (1) println 사용해서 출력하기

---
+++ src/main/kotlin/playground/KCalendar.kt
@@ -0,0 +1,10 @@
+package playground
+
+fun main() {
+    println("일 월 화 수 목 금 토")
+    println("--------------------")
+    println("1  2  3  4  5  6  7")
+    println("8  9 10 11 12 13 14")
+    println("15 16 17 18 19 20 21")
+    println("22 23 24 25 26 27 28")
+}
