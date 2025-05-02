7ffa4c3331d477baa48ebbc48bb5ed071f77a39f
TaeGeun Jang <devmeeple@gmail.com>
2025-05-02 22:35:09 +0900

    2.3 4, 5단 구현 - 값 입력 및 변수

--- src/main/java/playground/calculator/GugudanCalculatorApplication.java
+++ src/main/java/playground/calculator/GugudanCalculatorApplication.java
@@ -1,29 +1,46 @@
 package playground.calculator;
 
+import java.util.Scanner;
+
 public class GugudanCalculatorApplication {
     public static void main(String[] args) {
+        System.out.println("[구구단 프로그램] \n");
 
-        System.out.println("구구단 계산기");
-        System.out.println("---------------");
-        System.out.println("2 * 1 = 2");
-        System.out.println("2 * 2 = 4");
-        System.out.println("2 * 3 = 6");
-        System.out.println("2 * 4 = 8");
-        System.out.println("2 * 5 = 10");
-        System.out.println("2 * 6 = 12");
-        System.out.println("2 * 7 = 14");
-        System.out.println("2 * 8 = 16");
-        System.out.println("2 * 9 = 18");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Q. 몇 단을 출력할까요?");
+        System.out.print("A. 입력: ");
+        final int dan = scanner.nextInt();
+        scanner.close();
 
-        System.out.println("---------------");
-        System.out.println("3 * 1 = 3");
-        System.out.println("3 * 2 = 6");
-        System.out.println("3 * 3 = 9");
-        System.out.println("3 * 4 = 12");
-        System.out.println("3 * 5 = 15");
-        System.out.println("3 * 6 = 18");
-        System.out.println("3 * 7 = 21");
-        System.out.println("3 * 8 = 24");
-        System.out.println("3 * 9 = 27");
+        System.out.println();
+        System.out.printf("<%d단 출력>\n", dan);
+
+        int result = dan * 1;
+        System.out.printf("%d * 1 = %d\n", dan, result);
+
+        result = dan * 2;
+        System.out.printf("%d * 2 = %d\n", dan, result);
+
+        result = dan * 3;
+        System.out.printf("%d * 3 = %d\n", dan, result);
+
+        result = dan * 4;
+        System.out.printf("%d * 4 = %d\n", dan, result);
+
+        result = dan * 5;
+        System.out.printf("%d * 5 = %d\n", dan, result);
+
+        result = dan * 6;
+        System.out.printf("%d * 6 = %d\n", dan, result);
+
+        result = dan * 7;
+        System.out.printf("%d * 7 = %d\n", dan, result);
+
+        result = dan * 8;
+        System.out.printf("%d * 8 = %d\n", dan, result);
+
+        result = dan * 9;
+        System.out.printf("%d * 9 = %d\n", dan, result);
+
     }
 }
