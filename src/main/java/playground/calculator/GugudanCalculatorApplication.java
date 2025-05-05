eed1ccc0748188e9bb229990fd300f16e3950443
TaeGeun Jang <devmeeple@gmail.com>
2025-05-05 22:02:51 +0900

    2.6 배열로 구구단 다시 구현

--- src/main/java/playground/calculator/GugudanCalculatorApplication.java
+++ src/main/java/playground/calculator/GugudanCalculatorApplication.java
@@ -1,26 +1,19 @@
 package playground.calculator;
 
-import java.util.Scanner;
-
 public class GugudanCalculatorApplication {
     public static void main(String[] args) {
-        System.out.println("[구구단 프로그램] \n");
+        for (int dan = 2; dan <= 9; dan++) {
+            int[] result = new int[9];
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Q. 몇 단을 출력할까요?");
-        System.out.print("A. 입력: ");
-        final int dan = scanner.nextInt();
-        scanner.close();
-
-        if (dan < 2 || dan > 9) {
-            System.out.println("2이상, 9이하의 숫자만 입력할 수 있습니다.");
-        } else {
-            System.out.println();
-            System.out.printf("<%d단 출력>\n", dan);
-
-            for (int i = 1; i < 10; i++) {
-                System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+            for (int i = 0; i < result.length; i++) {
+                result[i] = dan * (i + 1);
             }
+
+            System.out.println("=== " + dan + "단 ===");
+            for (int i = 0; i < result.length; i++) {
+                System.out.println(dan + " X " + (i + 1) + " = " + result[i]);
+            }
+            System.out.println();
         }
     }
 }
