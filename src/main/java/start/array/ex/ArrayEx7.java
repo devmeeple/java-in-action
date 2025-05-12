539a0ff452d0146800450b51aa2ac08fa32ac012
TaeGeun Jang <devmeeple@gmail.com>
2025-05-13 00:15:02 +0900

    배열 문제 (7) 2차원 배열1

---
+++ src/main/java/start/array/ex/ArrayEx7.java
@@ -0,0 +1,34 @@
+package start.array.ex;
+
+import java.util.Scanner;
+
+/**
+ * // TODO: 2025-05-13 실행 결과를 먼저 이해하고 시도하기
+ * 문제2: 2차원 배열1
+ * 1) 사용자로부터 학생 4명의 국어, 수학, 영어 점수 입력받기
+ * 2) 각 학생의 총점과 평균 계산
+ */
+public class ArrayEx7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] scores = new int[4][3];
+        String[] subjects = {"국어", "영어", "수학"};
+
+        for (int row = 0; row < scores.length; row++) {
+            System.out.println((row + 1) + "번 학생의 성적을 입력하세요:");
+            for (int column = 0; column < scores[row].length; column++) {
+                System.out.print(subjects[column] + " 점수:");
+                scores[row][column] = scanner.nextInt();
+            }
+        }
+
+        for (int row = 0; row < scores.length; row++) {
+            int total = 0;
+            for (int column = 0; column < scores[row].length; column++) {
+                total += scores[row][column];
+            }
+            double average = (double) total / scores[row].length;
+            System.out.println((row + 1) + "번 학생의 총점: " + total + ", 평균: " + average);
+        }
+    }
+}
