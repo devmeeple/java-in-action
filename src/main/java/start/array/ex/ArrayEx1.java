8268472eb85c1c9b3849a1e85e5bd689de71d3e9
장태근 <devmeeple@gmail.com>
2025-05-12 21:19:33 +0900

    배열 문제 (1) 향상된 for문을 사용하여 리팩터링

--- src/main/java/start/array/ex/ArrayEx1.java
+++ src/main/java/start/array/ex/ArrayEx1.java
@@ -8,8 +8,8 @@ public class ArrayEx1 {
         int[] students = {90, 80, 70, 60, 50};
 
         int total = 0;
-        for (int i = 0; i < students.length; i++) {
-            total += students[i];
+        for (int student : students) {
+            total += student;
         }
 
         double average = (double) total / students.length;
