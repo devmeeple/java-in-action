29d11d69ef43fecd7f9ad53cb23f2c5275c268b8
TaeGeun Jang <devmeeple@gmail.com>
2025-05-31 23:51:57 +0900

    중첩 클래스, 내부 클래스2 문제 (1) 정적 중첩 클래스를 완성해라

---
+++ src/main/java/mid1/nested/test/OuterClass1Main.java
@@ -0,0 +1,13 @@
+package mid1.nested.test;
+
+/**
+ * // TODO: 2025-05-31
+ * 문제1: 정적 중첩 클래스를 완성해라
+ * 1) NestedClass hello() 메서드 호출
+ */
+public class OuterClass1Main {
+    public static void main(String[] args) {
+        OuterClass1.NestedClass nested = new OuterClass1.NestedClass();
+        nested.hello();
+    }
+}
