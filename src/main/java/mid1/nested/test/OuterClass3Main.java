02ec8d1d74bbbca1abfa0ea3650f16274afaaf8a
TaeGeun Jang <devmeeple@gmail.com>
2025-06-01 23:53:32 +0900

    중첩 클래스, 내부 클래스2 문제 (3) 지역 클래스를 완성해라

---
+++ src/main/java/mid1/nested/test/OuterClass3Main.java
@@ -0,0 +1,12 @@
+package mid1.nested.test;
+
+/**
+ * 문제3: 지역 클래스를 완성해라
+ * 1) 지역 클래스 LocalClass hello() 메서드 호출
+ */
+public class OuterClass3Main {
+    public static void main(String[] args) {
+        OuterClass3 outerClass3 = new OuterClass3();
+        outerClass3.myMethod();
+    }
+}
