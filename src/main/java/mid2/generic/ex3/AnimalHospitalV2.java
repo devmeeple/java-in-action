455f763d85b3769d409061be1c21a85b2fc0139c
TaeGeun Jang <devmeeple@gmail.com>
2025-06-02 18:33:08 +0900

    제네릭(Generic2) - 타입 매개변수 제한3(제네릭 도입과 실패)

---
+++ src/main/java/mid2/generic/ex3/AnimalHospitalV2.java
@@ -0,0 +1,22 @@
+package mid2.generic.ex3;
+
+public class AnimalHospitalV2<T> {
+    private T animal;
+
+    public void checkup() {
+        animal.toString();
+        animal.equals(null);
+
+//        System.out.println("동물 이름: " + animal.getName());
+//        animal.sound();
+    }
+
+    public T getBigger(T target) {
+//        return animal.getSize() > target.getSize() ? animal : target;
+        return null;
+    }
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+}
