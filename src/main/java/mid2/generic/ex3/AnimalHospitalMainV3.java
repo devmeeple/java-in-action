d4429c3209b6753bc17681439415da713e9152f2
TaeGeun Jang <devmeeple@gmail.com>
2025-06-02 18:38:19 +0900

    제네릭(Generic2) - 타입 매개변수 제한4(타입 매개변수 제한)

---
+++ src/main/java/mid2/generic/ex3/AnimalHospitalMainV3.java
@@ -0,0 +1,26 @@
+package mid2.generic.ex3;
+
+import mid2.generic.animal.Cat;
+import mid2.generic.animal.Dog;
+
+public class AnimalHospitalMainV3 {
+    public static void main(String[] args) {
+        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3<>();
+        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3<>();
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("냐옹이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+//        dogHospital.set(cat);
+
+        dogHospital.set(dog);
+        Dog biggerDog = dogHospital.getBigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog = " + biggerDog);
+    }
+}
