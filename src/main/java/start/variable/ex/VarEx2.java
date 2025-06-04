bbf2c75cd26a77aa78ea13dc566d8637b86a3ba8
TaeGeun Jang <devmeeple@gmail.com>
2025-06-05 08:41:27 +0900

    변수 문제 (2)

---
+++ src/main/java/start/variable/ex/VarEx2.java
@@ -0,0 +1,18 @@
+package start.variable.ex;
+
+/**
+ * 문제2
+ * 1) 변수 num1을 선언, 10을 할당
+ * 2) 변수 num2를 선언, 20을 할당
+ * 3) 두 변수의 합을 저장하는 변수 sum 선언
+ * 4) 변수 sum의 값을 출력
+ */
+public class VarEx2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println(sum);
+    }
+}
