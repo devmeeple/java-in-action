82b848b322afa8606801a104146d910b9fd8e988
TaeGeun Jang <devmeeple@gmail.com>
2025-06-05 08:39:44 +0900

    변수 문제 (1)

---
+++ src/main/java/start/variable/ex/VarEx1Question.java
@@ -0,0 +1,13 @@
+package start.variable.ex;
+
+/**
+ * 문제1
+ * 1) 반복해서 사용하는 숫자 4, 3을 변수 num1, num2로 변경
+ */
+public class VarEx1Question {
+    public static void main(String[] args) {
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+    }
+}
