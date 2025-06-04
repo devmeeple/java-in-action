c90c0f5e2cdfc715166e592ce83822b63416c421
TaeGeun Jang <devmeeple@gmail.com>
2025-06-05 08:50:43 +0900

    연산자 - 문자열 더하기

--- src/main/java/start/operator/Operator2.java
+++ src/main/java/start/operator/Operator2.java
@@ -1,23 +1,21 @@
 package start.operator;
 
 public class Operator2 {
-
     public static void main(String[] args) {
-
-        String result1 = "hello" + "world";
-        System.out.println("result1 = " + result1);
+        String result1 = "hello " + "world";
+        System.out.println(result1);
 
         String s1 = "string1";
         String s2 = "string2";
         String result2 = s1 + s2;
-        System.out.println("result2 = " + result2);
+        System.out.println(result2);
 
-        String s3 = "a + b" + 10;
-        System.out.println("s3 = " + s3);
+        String result3 = "a + b" + 10;
+        System.out.println(result3);
 
         int num = 20;
         String str = "a + b = ";
         String result4 = str + num;
-        System.out.println("result4 = " + result4);
+        System.out.println(result4);
     }
 }
