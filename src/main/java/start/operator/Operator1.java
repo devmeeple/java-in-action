72debadf00011ae6d04b8c2efeb7ecdff5c0eb24
TaeGeun Jang <devmeeple@gmail.com>
2025-06-05 08:48:00 +0900

    연산자 - 산술 연산자

--- src/main/java/start/operator/Operator1.java
+++ src/main/java/start/operator/Operator1.java
@@ -1,24 +1,23 @@
 package start.operator;
 
 public class Operator1 {
-
     public static void main(String[] args) {
         int a = 5;
         int b = 2;
 
         int sum = a + b;
-        System.out.println("sum = " + sum);
+        System.out.println("a + b = " + sum);
 
         int diff = a - b;
-        System.out.println("diff = " + diff);
-
-        int div = a / b;
-        System.out.println("div = " + div);
+        System.out.println("a - b = " + diff);
 
         int multi = a * b;
-        System.out.println("multi = " + multi);
+        System.out.println("a * b = " + multi);
+
+        int div = a / b;
+        System.out.println("a / b = " + div);
 
         int mod = a % b;
-        System.out.println("mod = " + mod);
+        System.out.println("a % b = " + mod);
     }
 }
