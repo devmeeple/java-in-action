462f3575dca4e37e571db1d062993463879e9d30
TaeGeun Jang <devmeeple@gmail.com>
2025-06-05 09:25:47 +0900

    조건문 - if문3(if문과 else if문)

--- src/main/java/start/cond/If5.java
+++ src/main/java/start/cond/If5.java
@@ -1,28 +1,25 @@
 package start.cond;
 
 /**
- * 온라인 쇼핑몰 할인 시스템
- *
- * 아이템 가격이 10000원 이상이면, 1000원 할인한다.
- * 나이가 10살 이하라면 1000원 할인한다.
+ * 문제: 온라인 쇼핑몰 할인 시스템
+ * 1) 아이템 가격이 10000원 이상이면, 1000원 할인한다.
+ * 2) 나이가 10살 이하라면 1000원 할인한다.
  */
 public class If5 {
-
     public static void main(String[] args) {
-        int discount = 0;
         int price = 10000;
         int age = 10;
+        int discount = 0;
 
         if (price >= 10000) {
-            discount += 1000;
-            System.out.println("10000원 이상 구매, 1000원 할인!");
+            discount = discount + 1000;
+            System.out.println("10000원 이상 구매, 1000원 할인");
         }
         if (age <= 10) {
-            discount += 1000;
-            System.out.println("어린이, 1000원 할인!");
+            discount = discount + 1000;
+            System.out.println("어린이 1000원 할인");
         }
 
-        System.out.println("===========================");
-        System.out.println("총 할인 금액 = " + discount + "원");
+        System.out.println("총 할인 금액: " + discount + "원");
     }
 }
