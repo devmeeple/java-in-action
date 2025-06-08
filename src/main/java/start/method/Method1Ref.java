3be3ca1bd69d3d120f4364fd4769679048e8ae80
TaeGeun Jang <devmeeple@gmail.com>
2025-06-09 05:36:08 +0900

    메서드 - 메서드 사용

--- src/main/java/start/method/Method1Ref.java
+++ src/main/java/start/method/Method1Ref.java
@@ -5,7 +5,7 @@ public class Method1Ref {
         int sum1 = add(5, 10);
         System.out.println("결과1 출력:" + sum1);
 
-        int sum2 = add(5, 10);
+        int sum2 = add(15, 20);
         System.out.println("결과2 출력:" + sum2);
     }
 
