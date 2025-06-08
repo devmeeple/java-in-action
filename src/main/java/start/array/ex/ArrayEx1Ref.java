92054deb3fb3e5cddb59317d03f0192d19b0ba4a
TaeGeun Jang <devmeeple@gmail.com>
2025-06-08 11:58:51 +0900

    배열 문제 (1) 배열을 사용하도록 변경

---
+++ src/main/java/start/array/ex/ArrayEx1Ref.java
@@ -0,0 +1,19 @@
+package start.array.ex;
+
+/**
+ * 문제1: 배열을 사용하도록 변경
+ */
+public class ArrayEx1Ref {
+    public static void main(String[] args) {
+        int[] students = {90, 80, 70, 60, 50};
+
+        int total = 0;
+        for (int student : students) {
+            total += student;
+        }
+
+        double average = (double) total / students.length;
+        System.out.println("점수 총합: " + total);
+        System.out.println("점수 평균: " + average);
+    }
+}
