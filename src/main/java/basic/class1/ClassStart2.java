429c8b33037c70d5569eab8730f49f4c2a9d131a
TaeGeun Jang <devmeeple@gmail.com>
2025-06-10 17:15:26 +0900

    클래스와 데이터 - 클래스가 필요한 이유

--- src/main/java/basic/class1/ClassStart2.java
+++ src/main/java/basic/class1/ClassStart2.java
@@ -5,12 +5,12 @@ package basic.class1;
  */
 public class ClassStart2 {
     public static void main(String[] args) {
-        String[] students = {"학생1", "학생2"};
-        int[] ages = {15, 16};
-        int[] grades = {90, 80};
+        String[] studentNames = {"학생1", "학생2"};
+        int[] studentAges = {15, 16};
+        int[] studentGrades = {90, 80};
 
-        for (int i = 0; i < students.length; i++) {
-            System.out.println("이름:" + students[i] + " 나이:" + ages[i] + " 성적:" + grades[i]);
+        for (int i = 0; i < studentNames.length; i++) {
+            System.out.println("이름:" + studentNames[i] + " 나이:" + studentAges[i] + " 성적:" + studentGrades[i]);
         }
     }
 }
