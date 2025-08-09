19293313b6832b5f31fab52c27d0a4e74430d420
TaeGeun Jang <devmeeple@gmail.com>
2025-08-09 23:56:02 +0900

    응집도 - 입출금 내역 목록 처리

--- real-world/src/main/java/ch02_bank_statements_analyzer/tobe/BankStatementAnalyzer.java
+++ real-world/src/main/java/ch02_bank_statements_analyzer/tobe/BankStatementAnalyzer.java
@@ -5,41 +5,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementAnalyzer {
     private static final String RESOURCES = "src/main/resources/";
+    private static final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
 
     public static void main(String[] args) throws IOException {
-        BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
-
         String fileName = args[0];
         Path path = Paths.get(RESOURCES + fileName);
         List<String> lines = Files.readAllLines(path);
 
         List<BankTransaction> bankTransactions = bankStatementParser.parseLineFromCSV(lines);
+        BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
-        System.out.println("모든 거래 내역의 합: " + calculateTotalAmount(bankTransactions));
-        System.out.println("1월 입출금 내역: " + selectInMonth(bankTransactions, Month.JANUARY));
+        collectSummary(bankStatementProcessor);
     }
 
-    private static double calculateTotalAmount(List<BankTransaction> bankTransactions) {
-        double total = 0d;
-        for (BankTransaction bankTransaction : bankTransactions) {
-            total += bankTransaction.getAmount();
-        }
-
-        return total;
-    }
-
-    private static List<BankTransaction> selectInMonth(List<BankTransaction> bankTransactions, Month month) {
-        List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
-        for (BankTransaction bankTransaction : bankTransactions) {
-            if (bankTransaction.getDate().getMonth() == month) {
-                bankTransactionsInMonth.add(bankTransaction);
-            }
-        }
-        return bankTransactionsInMonth;
+    private static void collectSummary(BankStatementProcessor bankStatementProcessor) {
+        System.out.println("모든 거래 내역의 합: " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("1월 총 입출금 내역: " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
+        System.out.println("2월 총 입출금 내역: " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
+        System.out.println("총 급여: " + bankStatementProcessor.calculateTotalForCategory("Salary"));
     }
 }
