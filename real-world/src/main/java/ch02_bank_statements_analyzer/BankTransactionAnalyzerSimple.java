d36e74f6b18199d87adc494ab1d9e24b40d8878b
TaeGeun Jang <devmeeple@gmail.com>
2025-08-09 23:20:23 +0900

    모든 거래 내역의 합

---
+++ real-world/src/main/java/ch02_bank_statements_analyzer/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,25 @@
+package ch02_bank_statements_analyzer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(RESOURCES + args[0]);
+        List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+
+        for (String line : lines) {
+            String[] columns = line.split(",");
+            double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("모든 거래 내역의 합: " + total);
+    }
+}
