2aefbc90d2799b6e343059e42aa27fba8bdcddef
TaeGeun Jang <devmeeple@gmail.com>
2025-09-20 16:00:56 +0900

    배열 - 다차원 배열

---
+++ javajungsuk/src/main/java/ch05/ArrayEx18.java
@@ -0,0 +1,26 @@
+package ch05;
+
+public class ArrayEx18 {
+    public static void main(String[] args) {
+        int[][] scores = {
+                {100, 100, 100}
+                , {20, 20, 20}
+                , {30, 30, 30}
+                , {40, 40, 40}
+        };
+        int sum = 0;
+
+        for (int i = 0; i < scores.length; i++) {
+            for (int j = 0; j < scores[i].length; j++) {
+                System.out.printf("score[%d][%d] = %d%n", i, j, scores[i][j]);
+            }
+        }
+
+        for (int[] tmp : scores) {
+            for (int score : tmp) {
+                sum += score;
+            }
+        }
+        System.out.println("sum = " + sum);
+    }
+}
