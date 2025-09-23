f3a4db9ee170cdc8e8806bec9d99a98e9901f074
TaeGeun Jang <devmeeple@gmail.com>
2025-09-23 23:53:50 +0900

    객체지향 프로그래밍 1 - 메서드의 호출

---
+++ javajungsuk/src/main/java/ch06/MyMathEx.java
@@ -0,0 +1,37 @@
+package ch06;
+
+public class MyMathEx {
+    public static void main(String[] args) {
+        MyMath myMath = new MyMath();
+
+        long result1 = myMath.add(5L, 3L);
+        long result2 = myMath.subtract(5L, 3L);
+        long result3 = myMath.multiply(5L, 3L);
+        double result4 = myMath.divide(5L, 3L);
+
+        System.out.println("add(5L, 3L) = " + result1);
+        System.out.println("subtract(5L, 3L) = " + result2);
+        System.out.println("multiply(5L, 3L) = " + result3);
+        System.out.println("divide(5L, 3L) = " + result4);
+    }
+}
+
+class MyMath {
+    long add(long a, long b) {
+        long result = a + b;
+        return result;
+//        return a + b;
+    }
+
+    long subtract(long a, long b) {
+        return a - b;
+    }
+
+    long multiply(long a, long b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        return a / b;
+    }
+}
