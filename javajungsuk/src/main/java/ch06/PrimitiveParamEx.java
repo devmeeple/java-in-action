351995fb0c5d6e1bfd3e1156f892b1ede8b2c42e
장태근 <devmeeple@gmail.com>
2025-09-24 23:38:38 +0900

    객체지향 프로그래밍 1 - 기본형 매개변수와 참조형 매개변수

---
+++ javajungsuk/src/main/java/ch06/PrimitiveParamEx.java
@@ -0,0 +1,24 @@
+package ch06;
+
+public class PrimitiveParamEx {
+    public static void main(String[] args) {
+        Data data = new Data();
+        data.x = 10;
+
+        System.out.println("main() : x = " + data.x);
+
+        change(data.x);
+
+        System.out.println("After change(data.x)");
+        System.out.println("main() : x = " + data.x);
+    }
+
+    static void change(int x) {
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+}
+
+class Data {
+    int x;
+}
