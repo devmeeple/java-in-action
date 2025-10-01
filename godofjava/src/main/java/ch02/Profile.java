1aa87222597c539a6cd6f39b0f3f20fea4104e56
TaeGeun Jang <devmeeple@gmail.com>
2025-10-01 17:56:14 +0900

    2장 Hello God Of Java

---
+++ godofjava/src/main/java/ch02/Profile.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Profile {
+    public static void main(String[] args) {
+        String name = "James Arthur Gosling";
+        int age = 70;
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+    }
+}
