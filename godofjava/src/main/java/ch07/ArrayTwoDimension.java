b0bd71beaaee29d899245978ca5f0e5f09e9d8c2
장태근 <devmeeple@gmail.com>
2025-10-12 23:50:29 +0900

    2차원 배열

---
+++ godofjava/src/main/java/ch07/ArrayTwoDimension.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class ArrayTwoDimension {
+    public static void main(String[] args) {
+        ArrayTwoDimension array = new ArrayTwoDimension();
+        array.twoDimensionArray();
+    }
+
+    public void twoDimensionArray() {
+        int[][] twoDim;
+        twoDim = new int[2][3];
+    }
+}
