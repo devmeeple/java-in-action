4d029f634a75ffcc482c1c57ab1160ceca33742d
장태근 <devmeeple@gmail.com>
2025-10-12 23:39:32 +0900

    배열을 선언하는 방법

---
+++ godofjava/src/main/java/ch07/ArrayInitialize.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class ArrayInitialize {
+    public static void main(String[] args) {
+        ArrayInitialize array = new ArrayInitialize();
+        array.otherInit();
+    }
+
+    public void otherInit() {
+        int[] lottoNumbers = {5, 12, 23, 25, 38, 41, 2};
+//        int[] lottoNumbers2;
+//        lottoNumbers2 = {5, 12, 23, 25, 38, 41, 2}; // compile error
+    }
+}
