8d044bcbd04020d700d952f4d1b6d41a07fd9a20
장태근 <devmeeple@gmail.com>
2025-10-12 23:53:30 +0900

    배열의 길이

---
+++ godofjava/src/main/java/ch07/ArrayLength.java
@@ -0,0 +1,16 @@
+package ch07;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        ArrayLength array = new ArrayLength();
+        array.printArrayLength();
+    }
+
+    public void printArrayLength() {
+        int[] oneDim = new int[3];
+        int[][] twoDim = new int[4][2];
+
+        System.out.println(oneDim.length);
+        System.out.println(twoDim.length);
+    }
+}
