8520b4d360aca9d1c4f06d47a782be576587cc82
장태근 <devmeeple@gmail.com>
2025-10-12 23:48:28 +0900

    배열을 선언하는 방법(클래스 변수)

--- godofjava/src/main/java/ch07/ArrayInitialize.java
+++ godofjava/src/main/java/ch07/ArrayInitialize.java
@@ -1,7 +1,7 @@
 package ch07;
 
 public class ArrayInitialize {
-    String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    static String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
     public static void main(String[] args) {
         ArrayInitialize array = new ArrayInitialize();
