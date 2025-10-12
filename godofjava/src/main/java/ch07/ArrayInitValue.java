fcb8ec262dff98e2468704d49f249dd9a07c4404
장태근 <devmeeple@gmail.com>
2025-10-12 23:33:00 +0900

    출력 순서 변경

--- godofjava/src/main/java/ch07/ArrayInitValue.java
+++ godofjava/src/main/java/ch07/ArrayInitValue.java
@@ -18,14 +18,14 @@ public class ArrayInitValue {
         char[] charArray = new char[1];
         boolean[] booleanArray = new boolean[1];
 
-        System.out.println("booleanArray[0] = " + booleanArray[0]);
+        System.out.println("byteArray[0] = " + byteArray[0]);
         System.out.println("shortArray[0] = " + shortArray[0]);
         System.out.println("intArray[0] = " + intArray[0]);
         System.out.println("longArray[0] = " + longArray[0]);
         System.out.println("floatArray[0] = " + floatArray[0]);
         System.out.println("doubleArray[0] = " + doubleArray[0]);
         System.out.println("charArray[0] = [" + charArray[0] + "]");
-        System.out.println("byteArray[0] = " + byteArray[0]);
+        System.out.println("booleanArray[0] = " + booleanArray[0]);
     }
 
     public void referenceTypes() {
