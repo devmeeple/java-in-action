5493f963f18b9776e4c974a463ee6bd853ea07ef
TaeGeun Jang <devmeeple@gmail.com>
2025-10-13 16:16:18 +0900

    메서드 오버로딩(Overloading)

---
+++ godofjava/src/main/java/ch08/ReferenceOverloading.java
@@ -0,0 +1,18 @@
+package ch08;
+
+public class ReferenceOverloading {
+    public static void main(String[] args) {
+    }
+
+    public void print(int data) {
+    }
+
+    public void print(String data) {
+    }
+
+    public void print(int intData, String stringData) {
+    }
+
+    public void print(String stringData, int intData) {
+    }
+}
