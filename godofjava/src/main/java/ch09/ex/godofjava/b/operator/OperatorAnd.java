326bc68f36c1b3bab78f5c2776feeeae4596d58c
TaeGeun Jang <devmeeple@gmail.com>
2025-10-14 00:02:26 +0900

    9. 자바를 배우면 패키지와 접근 제어자는 꼭 알아야 해요

---
+++ godofjava/src/main/java/ch09/ex/godofjava/b/operator/OperatorAnd.java
@@ -0,0 +1,11 @@
+package ch09.ex.godofjava.b.operator;
+
+public class OperatorAnd {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 1;
+
+        System.out.println((a == 3) && (b == 1));
+        System.out.println((a == 3) & (b == 1));
+    }
+}
---
+++ godofjava/src/main/java/ch09/ex/godofjava/b/variable/VariableTypes.java
@@ -0,0 +1,10 @@
+package ch09.ex.godofjava.b.variable;
+
+public class VariableTypes {
+    int instanceVariable;
+    static int classVariable;
+
+    public void method(int parameter) {
+        int localVariable;
+    }
+}
