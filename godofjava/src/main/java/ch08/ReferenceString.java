2ef5707dbe920abb9f7234b089d7ea6909495a69
TaeGeun Jang <devmeeple@gmail.com>
2025-10-13 16:07:39 +0900

    매개변수를 갖는 생성자(컴파일 에러)

---
+++ godofjava/src/main/java/ch08/ReferenceString.java
@@ -0,0 +1,11 @@
+package ch08;
+
+public class ReferenceString {
+    ReferenceString(String arg) {
+
+    }
+
+    public static void main(String[] args) {
+//        ReferenceString reference = new ReferenceString();
+    }
+}
