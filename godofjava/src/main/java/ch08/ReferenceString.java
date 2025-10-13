d8bfeba8719b000386184b55c30731daad31eb88
TaeGeun Jang <devmeeple@gmail.com>
2025-10-13 16:08:49 +0900

    매개변수를 갖는 생성자(컴파일 에러 해결) - 기본 생성자 추가

--- godofjava/src/main/java/ch08/ReferenceString.java
+++ godofjava/src/main/java/ch08/ReferenceString.java
@@ -1,11 +1,13 @@
 package ch08;
 
 public class ReferenceString {
+    ReferenceString() {
+    }
+    
     ReferenceString(String arg) {
-
     }
 
     public static void main(String[] args) {
-//        ReferenceString reference = new ReferenceString();
+        ReferenceString reference = new ReferenceString();
     }
 }
