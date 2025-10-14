915301756d3978f23f7aa3f75c3c89978e459271
TaeGeun Jang <devmeeple@gmail.com>
2025-10-14 09:16:28 +0900

    메서드 오버라이딩(Overriding)

---
+++ godofjava/src/main/java/ch10/c/inheritance/ChildOverriding.java
@@ -0,0 +1,11 @@
+package ch10.c.inheritance;
+
+public class ChildOverriding extends ParentOverriding {
+    public ChildOverriding() {
+        System.out.println("ChildOverriding Constructor");
+    }
+
+    public void printName() {
+        System.out.println("ChildOverriding printName()");
+    }
+}
