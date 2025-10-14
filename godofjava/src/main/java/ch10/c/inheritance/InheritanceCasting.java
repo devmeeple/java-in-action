23eff5d5e4d64cc319a1ca70748eebb6b75fae38
TaeGeun Jang <devmeeple@gmail.com>
2025-10-14 09:30:23 +0900

    참조 자료형의 형 변환(컴파일 에러)

---
+++ godofjava/src/main/java/ch10/c/inheritance/InheritanceCasting.java
@@ -0,0 +1,16 @@
+package ch10.c.inheritance;
+
+public class InheritanceCasting {
+    public static void main(String[] args) {
+        InheritanceCasting inheritance = new InheritanceCasting();
+        inheritance.objectCast();
+    }
+
+    public void objectCast() {
+        ParentCasting parent = new ParentCasting();
+        ChildCasting child = new ChildCasting();
+
+        ParentCasting parent2 = child;
+//        ChildCasting child2 = parent;
+    }
+}
