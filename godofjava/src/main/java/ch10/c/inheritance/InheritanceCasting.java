8538cce8cf4c849c632edcf043ff42c53ef9e16e
TaeGeun Jang <devmeeple@gmail.com>
2025-10-14 09:39:27 +0900

    instanceof 객체의 타입 확인하기

--- godofjava/src/main/java/ch10/c/inheritance/InheritanceCasting.java
+++ godofjava/src/main/java/ch10/c/inheritance/InheritanceCasting.java
@@ -3,12 +3,27 @@ package ch10.c.inheritance;
 public class InheritanceCasting {
     public static void main(String[] args) {
         InheritanceCasting inheritance = new InheritanceCasting();
-        inheritance.objectCast2();
+        inheritance.objectCastArray();
     }
 
-    public void objectCast2() {
-        ChildCasting child = new ChildCasting();
-        ParentCasting parent2 = child;
-        ChildCasting child2 = (ChildCasting) parent2;
+    public void objectCastArray() {
+        ParentCasting[] parentArray = new ParentCasting[3];
+        parentArray[0] = new ChildCasting();
+        parentArray[1] = new ParentCasting();
+        parentArray[2] = new ChildCasting();
+
+        objectTypeCheck(parentArray);
+    }
+
+    private void objectTypeCheck(ParentCasting[] parentArray) {
+        for (ParentCasting temParent : parentArray) {
+            if (temParent instanceof ChildCasting) {
+                System.out.println("ChildCasting");
+                ChildCasting tempChild = (ChildCasting) temParent;
+                tempChild.printAge();
+            } else if (temParent instanceof ParentCasting) {
+                System.out.println("ParentCasting");
+            }
+        }
     }
 }
