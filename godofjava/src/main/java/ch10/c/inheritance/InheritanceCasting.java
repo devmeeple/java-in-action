61fc2505716dd8741643d65ccbd9344b37c0bff2
TaeGeun Jang <devmeeple@gmail.com>
2025-10-14 09:33:38 +0900

    참조 자료형의 형 변환(명시적 형 변환 성공)

--- godofjava/src/main/java/ch10/c/inheritance/InheritanceCasting.java
+++ godofjava/src/main/java/ch10/c/inheritance/InheritanceCasting.java
@@ -3,14 +3,12 @@ package ch10.c.inheritance;
 public class InheritanceCasting {
     public static void main(String[] args) {
         InheritanceCasting inheritance = new InheritanceCasting();
-        inheritance.objectCast();
+        inheritance.objectCast2();
     }
 
-    public void objectCast() {
-        ParentCasting parent = new ParentCasting();
+    public void objectCast2() {
         ChildCasting child = new ChildCasting();
-
         ParentCasting parent2 = child;
-        ChildCasting child2 = (ChildCasting) parent;
+        ChildCasting child2 = (ChildCasting) parent2;
     }
 }
