3d75495fbb8ebf953589fc0cfd452ed5b05353de
TaeGeun Jang <devmeeple@gmail.com>
2025-10-15 10:31:07 +0900

    잘못된 equals를 사용한 객체 비교

--- godofjava/src/main/java/ch12/Equals.java
+++ godofjava/src/main/java/ch12/Equals.java
@@ -3,14 +3,14 @@ package ch12;
 public class Equals {
     public static void main(String[] args) {
         Equals thisObject = new Equals();
-        thisObject.equalMethod();
+        thisObject.equalMethod2();
     }
 
-    public void equalMethod() {
+    public void equalMethod2() {
         MemberDTO obj1 = new MemberDTO("Taegeun");
         MemberDTO obj2 = new MemberDTO("Taegeun");
 
-        if (obj1 == obj2) {
+        if (obj1.equals(obj2)) {
             System.out.println("obj1 and obj2 is same");
         } else {
             System.out.println("obj1 and obj2 is different");
