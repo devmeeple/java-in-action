1bd62b48ed44cfce96254ac17d571dc732ffe828
TaeGeun Jang <devmeeple@gmail.com>
2025-10-15 13:58:10 +0900

    예외 발생

---
+++ godofjava/src/main/java/ch14/exception/ExceptionSample.java
@@ -0,0 +1,13 @@
+package ch14.exception;
+
+public class ExceptionSample {
+    public static void main(String[] args) {
+        ExceptionSample sample = new ExceptionSample();
+        sample.arrayOutOfBounds();
+    }
+
+    public void arrayOutOfBounds() {
+        int[] intArray = new int[5];
+        System.out.println(intArray[5]);
+    }
+}
