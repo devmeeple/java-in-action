9975f2893a2bf0cf557786ecb7ba51bcf58e68c0
TaeGeun Jang <devmeeple@gmail.com>
2025-10-15 14:09:43 +0900

    try-catch

--- godofjava/src/main/java/ch14/exception/ExceptionSample.java
+++ godofjava/src/main/java/ch14/exception/ExceptionSample.java
@@ -3,11 +3,23 @@ package ch14.exception;
 public class ExceptionSample {
     public static void main(String[] args) {
         ExceptionSample sample = new ExceptionSample();
-        sample.arrayOutOfBounds();
+//        sample.arrayOutOfBounds();
+        sample.arrayOutOfBoundTryCatch();
     }
 
     public void arrayOutOfBounds() {
         int[] intArray = new int[5];
         System.out.println(intArray[5]);
+    }
+
+    public void arrayOutOfBoundTryCatch() {
+        int[] intArray = new int[5];
+        try {
+            System.out.println(intArray[5]);
+            System.out.println("This code should run.");
+        } catch (Exception e) {
+            System.err.println("Exception occured.");
+        }
+        System.out.println("This code must run.");
     }
 }
