febc7fc061109ad112c31cf6174698c16a5e746c
TaeGeun Jang <devmeeple@gmail.com>
2025-10-15 12:11:48 +0900

    변수에서 final 사용(참조형)

--- godofjava/src/main/java/ch13/model/MemberDTO.java
+++ godofjava/src/main/java/ch13/model/MemberDTO.java
@@ -5,6 +5,9 @@ public class MemberDTO {
     public String phone;
     public String email;
 
+    public MemberDTO() {
+    }
+
     public MemberDTO(String name) {
         this.name = name;
     }
