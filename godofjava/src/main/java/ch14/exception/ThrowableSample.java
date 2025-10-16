46e853b96867328594361190f62abcbfb6435d28
TaeGeun Jang <devmeeple@gmail.com>
2025-10-16 12:58:21 +0900

    예외 던지기(throws)

--- godofjava/src/main/java/ch14/exception/ThrowableSample.java
+++ godofjava/src/main/java/ch14/exception/ThrowableSample.java
@@ -1,20 +1,27 @@
 package ch14.exception;
 
 public class ThrowableSample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ThrowableSample sample = new ThrowableSample();
-        sample.throwable();
+        sample.throwException(13);
+        sample.throwsException(13);
     }
 
-    public void throwable() {
-        int[] intArray = new int[5];
+    public void throwException(int number) {
         try {
-            intArray = null;
-            System.out.println(intArray[5]);
-        } catch (Throwable t) {
-//            System.out.println(t.getMessage());
-//            System.out.println(t.toString());
-//            t.printStackTrace();
+            if (number > 12) {
+                throw new Exception("Number is over than 12");
+            }
+            System.out.println("Number is " + number);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void throwsException(int number) throws Exception {
+        if (number > 12) {
+            throw new Exception("Number is over tha 12");
+        }
+        System.out.println("Number is " + number);
     }
 }
