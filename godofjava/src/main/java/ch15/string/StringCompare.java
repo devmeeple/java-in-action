16e4c9f98395a09318b421d81998d0a7e2446017
TaeGeun Jang <devmeeple@gmail.com>
2025-10-16 13:44:47 +0900

    문자열의 길이(length)

---
+++ godofjava/src/main/java/ch15/string/StringCompare.java
@@ -0,0 +1,14 @@
+package ch15.string;
+
+public class StringCompare {
+    public static void main(String[] args) {
+        StringCompare sample = new StringCompare();
+        sample.checkString();
+    }
+
+    public void checkString() {
+        String text = "You must know String class.";
+
+        System.out.println("text.length() = " + text.length());
+    }
+}
