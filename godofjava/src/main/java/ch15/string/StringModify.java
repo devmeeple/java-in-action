a79eb56a341eafcc478a6a4cbace52e8fa2cf859
TaeGeun Jang <devmeeple@gmail.com>
2025-10-16 16:33:34 +0900

    특정 형식에 맞춰 값을 치환(format)

--- godofjava/src/main/java/ch15/string/StringModify.java
+++ godofjava/src/main/java/ch15/string/StringModify.java
@@ -3,16 +3,14 @@ package ch15.string;
 public class StringModify {
     public static void main(String[] args) {
         StringModify sample = new StringModify();
-        sample.checkReplace();
+        sample.checkFormat();
     }
-    
-    public void checkReplace() {
-        String text = "The String class represents character strings.";
 
-        System.out.println(text.replace('s', 'z'));
-        System.out.println(text);
-        System.out.println(text.replace("tring", "trike"));
-        System.out.println(text.replaceAll(" ", "|"));
-        System.out.println(text.replaceFirst(" ", "|"));
+    public void checkFormat() {
+        String text = "제 이름은 %s입니다. 지금까지 %d 권의 책을 썼고, "
+                + "하루에 %f %%의 시간을 책을 쓰는데 할애하고 있습니다.";
+        String realText = String.format(text, "이상민", 7, 10.5);
+//        String realText = String.format(text, "이상민", 7);
+        System.out.println(realText);
     }
 }
