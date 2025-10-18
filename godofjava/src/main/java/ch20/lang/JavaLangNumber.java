eeb23e2a4f184a03a4129d6dbe34a2a22485858e
TaeGeun Jang <devmeeple@gmail.com>
2025-10-18 18:24:42 +0900

    현재 시간 조회(currentTimeMills, nanoTime)

--- godofjava/src/main/java/ch20/lang/JavaLangNumber.java
+++ godofjava/src/main/java/ch20/lang/JavaLangNumber.java
@@ -13,4 +13,14 @@ public class JavaLangNumber {
         System.out.println("Integer HEX min = " + Integer.toHexString(Integer.MIN_VALUE));
         System.out.println("Integer HEX max = " + Integer.toHexString(Integer.MAX_VALUE));
     }
+
+    public void numberMinMaxCheck() {
+        System.out.println("Byte min = " + Byte.MIN_VALUE + ", max = " + Byte.MAX_VALUE);
+        System.out.println("Short min = " + Short.MIN_VALUE + ", max = " + Short.MAX_VALUE);
+        System.out.println("Integer min = " + Integer.MIN_VALUE + ", max = " + Integer.MAX_VALUE);
+        System.out.println("Long min = " + Long.MIN_VALUE + ", max = " + Long.MAX_VALUE);
+        System.out.println("Float min = " + Float.MIN_VALUE + ", max = " + Float.MAX_VALUE);
+        System.out.println("Double min = " + Double.MIN_VALUE + ", max = " + Double.MAX_VALUE);
+        System.out.println("Character min = " + (int) Character.MIN_VALUE + ", max = " + (int) Character.MAX_VALUE);
+    }
 }
--- godofjava/src/main/java/ch20/lang/JavaLangSystem.java
+++ godofjava/src/main/java/ch20/lang/JavaLangSystem.java
@@ -4,10 +4,21 @@ public class JavaLangSystem {
     public static void main(String[] args) {
         JavaLangSystem sample = new JavaLangSystem();
 
-        sample.systemPropertiesCheck();
+//        sample.systemPropertiesCheck();
+        sample.numberMinMaxElapsedCheck();
     }
 
     public void systemPropertiesCheck() {
         System.out.println("java.version = " + System.getProperty("java.version"));
+    }
+
+    public void numberMinMaxElapsedCheck() {
+        JavaLangNumber numberSample = new JavaLangNumber();
+        long startTime = System.currentTimeMillis();
+        long startNanoTime = System.nanoTime();
+
+        numberSample.numberMinMaxCheck();
+        System.out.println("Milli second = " + (System.currentTimeMillis() - startTime));
+        System.out.println("Nano second = " + (System.nanoTime() - startNanoTime));
     }
 }
