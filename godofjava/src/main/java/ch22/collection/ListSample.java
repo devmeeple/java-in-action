351c3f336a07dc9ce07a574c10bc1fa858964747
TaeGeun Jang <devmeeple@gmail.com>
2025-10-19 22:20:12 +0900

    ArrayList 데이터 꺼내기

--- godofjava/src/main/java/ch22/collection/ListSample.java
+++ godofjava/src/main/java/ch22/collection/ListSample.java
@@ -8,7 +8,10 @@ public class ListSample {
 
 //        sample.checkArrayList2();
 //        sample.checkArrayList3();
-        sample.checkArrayList4();
+//        sample.checkArrayList4();
+//        sample.checkArrayList5();
+//        sample.checkArrayList6();
+        sample.checkArrayList7();
     }
 
     public void checkArrayList2() {
@@ -54,6 +57,39 @@ public class ListSample {
         list1.add("Ooops");
         for (String tempData : list2) {
             System.out.println("List2 " + tempData);
+        }
+    }
+
+    public void checkArrayList5() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+
+        int listSize = list.size();
+        for (int loop = 0; loop < listSize; loop++) {
+            System.out.println("list.get(" + loop + ") = " + list.get(loop));
+
+        }
+    }
+
+    public void checkArrayList6() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+
+        String[] strList = list.toArray(new String[0]);
+        System.out.println(strList[0]);
+    }
+
+    public void checkArrayList7() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        String[] tempArray = new String[3];
+        String[] strList = list.toArray(tempArray);
+        for (String temp : strList) {
+            System.out.println(temp);
         }
     }
 }
