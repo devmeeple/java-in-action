9697d0f15c2359a3141e3994936d31e159e205dd
TaeGeun Jang <devmeeple@gmail.com>
2025-10-19 18:35:45 +0900

    제네릭(Generic) - 형변환 시 발생하는 문제를 사전에 방지

---
+++ godofjava/src/main/java/ch21/generic/GenericSample.java
@@ -0,0 +1,20 @@
+package ch21.generic;
+
+public class GenericSample {
+    public static void main(String[] args) {
+        GenericSample sample = new GenericSample();
+
+        sample.checkGenericDTO();
+    }
+
+    public void checkGenericDTO() {
+        CastingGenericDTO<String> dto1 = new CastingGenericDTO<>();
+        dto1.setObject(new String());
+
+        CastingGenericDTO<StringBuffer> dto2 = new CastingGenericDTO<>();
+        dto2.setObject(new StringBuffer());
+
+        CastingGenericDTO<StringBuilder> dto3 = new CastingGenericDTO<>();
+        dto3.setObject(new StringBuilder());
+    }
+}
