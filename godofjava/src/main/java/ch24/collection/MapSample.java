044dee765a8ae4f4bc9e05c0908cd24b13e3694b
TaeGeun Jang <devmeeple@gmail.com>
2025-10-20 12:06:18 +0900

    HashMap 데이터 삭제하기

--- godofjava/src/main/java/ch24/collection/MapSample.java
+++ godofjava/src/main/java/ch24/collection/MapSample.java
@@ -6,20 +6,15 @@ public class MapSample {
     public static void main(String[] args) {
         MapSample sample = new MapSample();
 
-        sample.checkContains();
+        sample.checkRemove();
     }
 
-    public void checkContains() {
+    public void checkRemove() {
         HashMap<String, String> map = new HashMap<>();
-
         map.put("A", "a");
-        map.put("B", "b");
-        map.put("C", "c");
-        map.put("D", "d");
 
-        System.out.println(map.containsKey("A"));
-        System.out.println(map.containsKey("Z"));
-        System.out.println(map.containsValue("a"));
-        System.out.println(map.containsValue("z"));
+        map.remove("A");
+
+        System.out.println(map.size());
     }
 }
