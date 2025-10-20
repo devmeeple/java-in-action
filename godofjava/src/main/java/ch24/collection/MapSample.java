24da2d8e404eb5eaacfd78593264b8aba2868537
TaeGeun Jang <devmeeple@gmail.com>
2025-10-20 12:04:37 +0900

    HashMap 키, 값 포함 여부 확인하기

--- godofjava/src/main/java/ch24/collection/MapSample.java
+++ godofjava/src/main/java/ch24/collection/MapSample.java
@@ -1,56 +1,15 @@
 package ch24.collection;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class MapSample {
     public static void main(String[] args) {
         MapSample sample = new MapSample();
 
-//        sample.checkHashMap();
-//        sample.checkKeySet();
-//        sample.checkValues();
-        sample.checkHashMapEntry();
+        sample.checkContains();
     }
 
-    public void checkHashMap() {
-        HashMap<String, String> map = new HashMap<>();
-
-        map.put("A", "a");
-
-        System.out.println(map.get("A"));
-        System.out.println(map.get("B"));
-    }
-
-    public void checkKeySet() {
-        HashMap<String, String> map = new HashMap<>();
-
-        map.put("A", "a");
-        map.put("C", "c");
-        map.put("D", "d");
-
-        Set<String> keySet = map.keySet();
-        for (String tempKey : keySet) {
-            System.out.println(tempKey + " = " + map.get(tempKey));
-        }
-    }
-
-    public void checkValues() {
-        HashMap<String, String> map = new HashMap<>();
-
-        map.put("A", "a");
-        map.put("C", "c");
-        map.put("D", "d");
-
-        Collection<String> values = map.values();
-        for (String tempValue : values) {
-            System.out.println(tempValue);
-        }
-    }
-
-    public void checkHashMapEntry() {
+    public void checkContains() {
         HashMap<String, String> map = new HashMap<>();
 
         map.put("A", "a");
@@ -58,9 +17,9 @@ public class MapSample {
         map.put("C", "c");
         map.put("D", "d");
 
-        Set<Map.Entry<String, String>> entries = map.entrySet();
-        for (Map.Entry<String, String> tempEntry : entries) {
-            System.out.println(tempEntry.getKey() + " = " + tempEntry.getValue());
-        }
+        System.out.println(map.containsKey("A"));
+        System.out.println(map.containsKey("Z"));
+        System.out.println(map.containsValue("a"));
+        System.out.println(map.containsValue("z"));
     }
 }
