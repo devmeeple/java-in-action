3076652c05eb6fe4708e4caac8e786222faa3258
TaeGeun Jang <devmeeple@gmail.com>
2025-10-21 10:39:56 +0900

    데몬 스레드(Daemon Thread)

--- godofjava/src/main/java/ch25/thread/RunDaemonThreads.java
+++ godofjava/src/main/java/ch25/thread/RunDaemonThreads.java
@@ -4,29 +4,13 @@ public class RunDaemonThreads {
     public static void main(String[] args) {
         RunDaemonThreads sample = new RunDaemonThreads();
 
-        sample.checkThreadProperty();
+        sample.runCommonThread();
     }
 
-    public void checkThreadProperty() {
-        ThreadSample thread1 = new ThreadSample();
-        ThreadSample thread2 = new ThreadSample();
-        ThreadSample daemonThread = new ThreadSample();
+    public void runCommonThread() {
+        DaemonThread thread = new DaemonThread();
 
-//        getId() 메서드는 오버라이딩을 방지하여 스레드 ID의 고유성과 불변성을 강제 보장하기 위해 deprecated 처리되었다.
-//        System.out.println("thread1 id = " + thread1.getId());
-//        System.out.println("thread1 id = " + thread2.getId());
-        
-        System.out.println("thread1 id = " + thread1.threadId());
-        System.out.println("thread2 id = " + thread2.threadId());
-
-        System.out.println("thread1 name = " + thread1.getName());
-        System.out.println("thread2 name = " + thread2.getName());
-
-        System.out.println("thread1 priority = " + thread1.getPriority());
-
-        daemonThread.setDaemon(true);
-
-        System.out.println("thread1 isDaemon = " + thread1.isDaemon());
-        System.out.println("daemonThread isDaemon = " + daemonThread.isDaemon());
+        thread.setDaemon(true);
+        thread.start();
     }
 }
