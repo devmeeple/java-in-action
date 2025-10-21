6db6ecb515a86f694e94827e5e362861719cc6ce
TaeGeun Jang <devmeeple@gmail.com>
2025-10-21 14:11:39 +0900

    파일 필터

---
+++ godofjava/src/main/java/ch26/io/FileFilterSample.java
@@ -0,0 +1,26 @@
+package ch26.io;
+
+import java.io.File;
+
+public class FileFilterSample {
+    public static void main(String[] args) {
+        FileFilterSample sample = new FileFilterSample();
+        String pathName = File.separator + "godofjava" + File.separator + "text";
+
+        sample.checkList(pathName);
+    }
+
+    public void checkList(String pathName) {
+        File file;
+        try {
+            file = new File(pathName);
+            File[] mainFileList = file.listFiles();
+//            File[] mainFileList = file.listFiles(new JPGFileFilter());
+            for (File tempFile : mainFileList) {
+                System.out.println(tempFile.getName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
