0a2317c44392702c4797bd9478bd60558e2c0a5d
TaeGeun Jang <devmeeple@gmail.com>
2025-11-17 15:20:34 +0900

    Gutter Game 기능 구현 테스트

---
+++ tdd-bowling-kata/src/main/java/bowling/Game.java
@@ -0,0 +1,11 @@
+package bowling;
+
+public class Game {
+
+    public void roll(int i) {
+    }
+
+    public int getScore() {
+        return 0;
+    }
+}
---
+++ tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
@@ -0,0 +1,21 @@
+package bowling;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BowlingGameTest {
+
+    @DisplayName("Gutter Game: 모든 투구가 0점인 경우, 최종 점수는 0점이다.")
+    @Test
+    void gutterGame() {
+        Game game = new Game();
+
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+        assertThat(game.getScore()).isEqualTo(0);
+    }
+}
