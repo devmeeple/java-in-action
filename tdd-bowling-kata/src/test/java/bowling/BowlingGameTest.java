0843eb46b64030709b479b3c99e68685623cde8e
TaeGeun Jang <devmeeple@gmail.com>
2025-11-17 20:10:45 +0900

    테스트 중복 제거 리팩터링
    - 객체 초기화
    - 헬퍼 메서드

--- tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
+++ tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package bowling;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,27 +8,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BowlingGameTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @DisplayName("Gutter Game: 모든 투구가 0점인 경우, 최종 점수는 0점이다.")
     @Test
     void gutterGame() {
-        Game game = new Game();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(20, 0);
 
         assertThat(game.getScore()).isEqualTo(0);
     }
 
-    @DisplayName("All Ones: 20번의 모든 투구가 1점인 경우, 최종 점수는 20점이다.")
+    @DisplayName("All Ones: 모든 투구가 1점인 경우, 최종 점수는 20점이다.")
     @Test
     void allOnes() {
-        Game game = new Game();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        rollMany(20, 1);
 
         assertThat(game.getScore()).isEqualTo(20);
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
     }
 }
