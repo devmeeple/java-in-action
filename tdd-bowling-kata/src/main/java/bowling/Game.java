90625490ef7b00e8f0a595bdcbed29c0eb844a9a
TaeGeun Jang <devmeeple@gmail.com>
2025-11-17 19:47:24 +0900

    All Ones 기능 구현 테스트

--- tdd-bowling-kata/src/main/java/bowling/Game.java
+++ tdd-bowling-kata/src/main/java/bowling/Game.java
@@ -1,11 +1,13 @@
 package bowling;
 
 public class Game {
+    private int score = 0;
 
-    public void roll(int i) {
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int getScore() {
-        return 0;
+        return score;
     }
 }
--- tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
+++ tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
@@ -18,4 +18,16 @@ class BowlingGameTest {
 
         assertThat(game.getScore()).isEqualTo(0);
     }
+
+    @DisplayName("All Ones: 20번의 모든 투구가 1점인 경우, 최종 점수는 20점이다.")
+    @Test
+    void allOnes() {
+        Game game = new Game();
+
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+
+        assertThat(game.getScore()).isEqualTo(20);
+    }
 }
