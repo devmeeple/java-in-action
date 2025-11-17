b867be8857d6026d6ca5ba05b55119abf8313e12
TaeGeun Jang <devmeeple@gmail.com>
2025-11-18 07:47:26 +0900

    메서드 이름 추상화

--- tdd-bowling-kata/src/main/java/bowling/Game.java
+++ tdd-bowling-kata/src/main/java/bowling/Game.java
@@ -8,20 +8,20 @@ public class Game {
         rolls[currentRoll++] = pins;
     }
 
-    public int getScore() {
+    public int score() {
         int score = 0;
         int frameIndex = 0;
 
         for (int frame = 0; frame < 10; frame++) {
             if (isStrike(frameIndex)) {
-                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                score += 10 + strikeBonus(frameIndex);
                 frameIndex += 1;
             }
             else if (isSpare(frameIndex)) {
-                score += 10 + rolls[frameIndex + 2];
+                score += 10 + spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                score += sumOfPinsInFrame(frameIndex);
                 frameIndex += 2;
             }
         }
@@ -33,7 +33,19 @@ public class Game {
         return rolls[frameIndex] == 10;
     }
 
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
     private boolean isSpare(int frameIndex) {
         return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int sumOfPinsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
     }
 }
--- tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
+++ tdd-bowling-kata/src/test/java/bowling/BowlingGameTest.java
@@ -20,7 +20,7 @@ class BowlingGameTest {
     void gutterGame() {
         rollMany(20, 0);
 
-        assertThat(game.getScore()).isEqualTo(0);
+        assertThat(game.score()).isEqualTo(0);
     }
 
     @DisplayName("All Ones: 모든 투구가 1점인 경우, 최종 점수는 20점이다.")
@@ -28,7 +28,7 @@ class BowlingGameTest {
     void allOnes() {
         rollMany(20, 1);
 
-        assertThat(game.getScore()).isEqualTo(20);
+        assertThat(game.score()).isEqualTo(20);
     }
 
     @DisplayName("One Spare: 첫 프레임 스페어(5,5) 후 3점 투구 시 총점은 16점이다.")
@@ -38,7 +38,7 @@ class BowlingGameTest {
         game.roll(3);
         rollMany(17, 0);
 
-        assertThat(game.getScore()).isEqualTo(16);
+        assertThat(game.score()).isEqualTo(16);
     }
 
     @DisplayName("One Strike: 첫 프레임 스트라이크 후 3점, 4점 투구 시 총점은 24점이다.")
@@ -49,7 +49,7 @@ class BowlingGameTest {
         game.roll(4);
         rollMany(16, 0);
 
-        assertThat(game.getScore()).isEqualTo(24);
+        assertThat(game.score()).isEqualTo(24);
     }
 
     private void rollStrike() {
