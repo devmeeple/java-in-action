5c0c371906bb2e6ea9ae6be7ea58f74291370062
TaeGeun Jang <devmeeple@gmail.com>
2025-11-18 18:23:59 +0900

    [Ch01] 쓰면서 제대로 공부하기 (2)

---
+++ head-first-java/src/main/java/ch01/DooBee.java
@@ -0,0 +1,17 @@
+package ch01;
+
+public class DooBee {
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x < 3) {
+            System.out.print("Dee");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3) {
+            System.out.print("Do");
+        }
+    }
+}
