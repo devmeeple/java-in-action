9d9fa458a220c8860bae4d4625cdd3acf89364b4
TaeGeun Jang <devmeeple@gmail.com>
2025-11-19 12:38:29 +0900

    [Ch03] 쓰면서 제대로 공부하기

---
+++ head-first-java/src/main/java/ch03/Sharpen.java
@@ -0,0 +1,19 @@
+package ch03;
+
+public class Sharpen {
+
+    void sharpen() {
+//        int x = 34.5;
+//        boolean boo = x;
+        int g = 17;
+        int y = g;
+        y = y + 10;
+        short s;
+//        s = y;
+        byte b = 3;
+        byte v = b;
+        short n = 12;
+//        v = n;
+//        byte k = 128;
+    }
+}
