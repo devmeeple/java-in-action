ba7669f3d6359a3537bc99891d335ee0282e3355
TaeGeun Jang <devmeeple@gmail.com>
2025-11-19 12:37:22 +0900

    [Ch03] 변수 선언과 대입 방법

---
+++ head-first-java/src/main/java/ch03/ExampleDeclarations.java
@@ -0,0 +1,49 @@
+package ch03;
+
+public class ExampleDeclarations {
+
+    void declarations() {
+        int x;
+        x = 234;
+        byte b = 89;
+        boolean isFun = true;
+        double d = 3456.98;
+        char c = 'f';
+        int z = x;
+        boolean isPunkRock;
+        isPunkRock = false;
+        boolean powerOn;
+        powerOn = isFun;
+        long big = 3456789L;
+        float f = 32.5f;
+    }
+
+    void declareLiterals() {
+        int x = 234;
+
+        int size = 32;
+        char initial = 'j';
+        double d = 456.709;
+        boolean isLearning;
+        isLearning = true;
+        int y = x + 456;
+    }
+
+    void invalidNames() {
+        int exports;
+        int module;
+        int open;
+        int opens;
+        int permits;
+        int provides;
+        int record;
+        int requires;
+        int sealed;
+        int to;
+        int transitive;
+        int uses;
+        int var;
+        int with;
+        int yield;
+    }
+}
