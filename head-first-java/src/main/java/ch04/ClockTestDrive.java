214949b582c40041fe189ef744ea2cd809515556
TaeGeun Jang <devmeeple@gmail.com>
2025-11-20 22:39:59 +0900

    [Ch04] 연습 문제

---
+++ head-first-java/src/main/java/ch04/ClockTestDrive.java
@@ -0,0 +1,23 @@
+package ch04;
+
+public class ClockTestDrive {
+
+    public static void main(String[] args) {
+        Clock c = new Clock();
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("time:" + tod);
+    }
+}
+
+class Clock {
+    String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
