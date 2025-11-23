50d06c8ed7b41a2258b55dd2c1e8442598ba6d3b
TaeGeun Jang <devmeeple@gmail.com>
2025-11-23 20:55:02 +0900

    [Ch06] 쓰면서 제대로 공부하기

---
+++ head-first-java/src/main/java/ch06/SharpenYourPencil.java
@@ -0,0 +1,48 @@
+package ch06;
+
+import java.util.ArrayList;
+
+public class SharpenYourPencil {
+
+    void arrayListCode() {
+        ArrayList<String> myList = new ArrayList<>();
+
+        String a = "whoohoo";
+        myList.add(a);
+
+        String b = "Frog";
+        myList.add(b);
+
+        int theSize = myList.size();
+
+        String str = myList.get(1);
+
+        myList.remove(1);
+
+        boolean isIn = myList.contains(b);
+    }
+
+    void arrayCode() {
+        String[] myList = new String[2];
+
+        String a = "whoohoo";
+        myList[0] = a;
+
+        String b = "Frog";
+        myList[1] = b;
+
+        int theSize = myList.length;
+
+        String str = myList[1];
+
+        myList[1] = null;
+
+        boolean isin = false;
+        for (String item : myList) {
+            if (b.equals(item)) {
+                isin = true;
+                break;
+            }
+        }
+    }
+}
