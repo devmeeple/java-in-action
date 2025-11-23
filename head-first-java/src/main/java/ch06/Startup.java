400e1a4f77e95cdba744937113352761ceb70146
TaeGeun Jang <devmeeple@gmail.com>
2025-11-23 20:51:53 +0900

    [Ch06] Startup 개선(ArrayList 활용)

---
+++ head-first-java/src/main/java/ch06/Startup.java
@@ -0,0 +1,34 @@
+package ch06;
+
+import java.util.ArrayList;
+
+public class Startup {
+
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + " : ( ");
+            } else{
+                result = "hit";
+            }
+        }
+        return result;
+    }
+
+    public void setLocationCells(ArrayList<String> locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
