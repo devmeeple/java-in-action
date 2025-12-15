b1433fff68fabae2cb81059afc50f8c6da7247e6
TaeGeun Jang <devmeeple@gmail.com>
2025-12-15 23:53:52 +0900

    AssertJ 라이브러리 추가

--- coding-test/build.gradle.kts
+++ coding-test/build.gradle.kts
@@ -9,8 +9,15 @@ repositories {
     mavenCentral()
 }
 
+val junitVersion = "5.10.0"
+val assertjVersion = "3.27.6"
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:$junitVersion"))
+
+    // 테스트 라이브러리 AssertJ Core
+    testImplementation("org.assertj:assertj-core:$assertjVersion")
+
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
