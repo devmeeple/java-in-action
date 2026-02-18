3555dc2ced683069ba0e3ce2949381d27a301a85
TaeGeun Jang <devmeeple@gmail.com>
2026-02-18 23:14:32 +0900

    [#11] P1011 문자 1개 입력받아 출력

---
+++ coding-test/src/main/java/codeup/basic/io/P1011.java
@@ -0,0 +1,13 @@
+package codeup.basic.io;
+
+import java.util.Scanner;
+
+public class P1011 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char x = scanner.next().charAt(0);
+
+        System.out.println(x);
+        scanner.close();
+    }
+}
