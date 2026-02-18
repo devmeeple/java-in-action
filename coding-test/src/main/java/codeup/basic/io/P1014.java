1a481f92d3bc905bed76ab06cb24f7146b6f6a47
TaeGeun Jang <devmeeple@gmail.com>
2026-02-18 23:40:03 +0900

    [#11] P1014 문자 2개 입력받아 순서 바꿔 출력

---
+++ coding-test/src/main/java/codeup/basic/io/P1014.java
@@ -0,0 +1,15 @@
+package codeup.basic.io;
+
+import java.util.Scanner;
+
+public class P1014 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char x = scanner.next().charAt(0);
+        char y = scanner.next().charAt(0);
+
+        System.out.printf("%c %c", y, x);
+        scanner.close();
+    }
+}
