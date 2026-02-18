5398fa7fc1fa814c4d8ce6e0b529ca27719ca391
TaeGeun Jang <devmeeple@gmail.com>
2026-02-19 00:36:11 +0900

    [#11] P1021 단어 1개 입력받아 그대로 출력

---
+++ coding-test/src/main/java/codeup/basic/io/P1021.java
@@ -0,0 +1,14 @@
+package codeup.basic.io;
+
+import java.util.Scanner;
+
+public class P1021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.next();
+        System.out.println(word);
+
+        scanner.close();
+    }
+}
