e7dc0e6f91c1a7e9bcb3ab645de2ea69f31061c8
TaeGeun Jang <devmeeple@gmail.com>
2026-02-18 23:10:47 +0900

    [#11] P1010 정수 1개 입력받아 출력

---
+++ coding-test/src/main/java/codeup/basic/io/P1010.java
@@ -0,0 +1,12 @@
+package codeup.basic.io;
+
+import java.util.Scanner;
+
+public class P1010 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(n);
+    }
+}
