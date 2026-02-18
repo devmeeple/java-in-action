8494d1775948944b84b0af853b988bc4d537e62b
TaeGeun Jang <devmeeple@gmail.com>
2026-02-19 00:38:34 +0900

    [#11] P1022 문장 1개 입력받아 출력

---
+++ coding-test/src/main/java/codeup/basic/io/P1022.java
@@ -0,0 +1,14 @@
+package codeup.basic.io;
+
+import java.util.Scanner;
+
+public class P1022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String sentence = scanner.nextLine();
+        System.out.println(sentence);
+
+        scanner.close();
+    }
+}
