baa1de4618088d385bda6123c75bbd7d8349f0dc
장태근 <devmeeple@gmail.com>
2026-02-19 20:22:15 +0900

    [#13] P1030 정수 1개 입력받아 그대로 출력3

---
+++ coding-test/src/main/java/codeup/basic/datatype/P1030.java
@@ -0,0 +1,14 @@
+package codeup.basic.datatype;
+
+import java.util.Scanner;
+
+public class P1030 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long result = scanner.nextLong();
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
