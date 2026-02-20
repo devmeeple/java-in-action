ac0f48728d2dfcaea02aaf9ef6d1763779a9f77c
TaeGeun Jang <devmeeple@gmail.com>
2026-02-20 11:05:45 +0900

    [#15] P1037 정수 입력받아 아스키 문자로 출력

---
+++ coding-test/src/main/java/codeup/basic/outputconversion/P1037.java
@@ -0,0 +1,16 @@
+package codeup.basic.outputconversion;
+
+import java.util.Scanner;
+
+public class P1037 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+        char result = (char) input;
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
