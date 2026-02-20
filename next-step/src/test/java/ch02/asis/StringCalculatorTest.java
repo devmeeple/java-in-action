e5073ce6da1682b70e6cb8cd19e7f0019ae58eb4
장태근 <devmeeple@gmail.com>
2026-02-20 20:38:15 +0900

    test: 문자열 계산기 테스트 코드 구현

---
+++ next-step/src/test/java/ch02/asis/StringCalculatorTest.java
@@ -0,0 +1,59 @@
+package ch02.asis;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StringCalculatorTest {
+
+    @DisplayName("빈 문자열이면 0을 반환한다")
+    @Test
+    void empty_returns_zero() {
+        String text = " ";
+
+        int result = StringCalculator.add(text);
+
+        assertEquals(0, result);
+    }
+
+    @DisplayName("쉼표 구분자를 지원한다")
+    @Test
+    void comma_separator() {
+        String text = "1,2";
+
+        int result = StringCalculator.add(text);
+
+        assertEquals(3, result);
+    }
+
+    @DisplayName("콜론 구분자를 지원한다")
+    @Test
+    void colon_separator() {
+        String text = "1,2";
+
+        int result = StringCalculator.add(text);
+
+        assertEquals(3, result);
+    }
+
+    @DisplayName("쉼표와 콜론을 함께 사용할 수 있다")
+    @Test
+    void mixed_separators() {
+        String text = "1,2:3";
+
+        int result = StringCalculator.add(text);
+
+        assertEquals(6, result);
+    }
+
+    @DisplayName("구분자 없이 숫자 하나면 해당 숫자를 반환한다")
+    @Test
+    void single_number() {
+        String text = "1";
+
+        int result = StringCalculator.add(text);
+
+        assertEquals(1, result);
+    }
+}
