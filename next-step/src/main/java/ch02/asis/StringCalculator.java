7090b5e13ab9ae33765e86f0057fda503f42461f
장태근 <devmeeple@gmail.com>
2026-02-20 20:37:48 +0900

    refactor: main 메서드를 add 메서드로 리팩터링

--- next-step/src/main/java/ch02/asis/StringCalculator.java
+++ next-step/src/main/java/ch02/asis/StringCalculator.java
@@ -4,21 +4,15 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
-    public static void main(String[] args) {
-        String text = "1,2:3";
-        int sum = 0;
-
+    public static int add(String text) {
         if (text.isBlank()) {
-            System.out.println(sum);
-            return;
+            return 0;
         }
 
         String[] tokens = text.split("[,:]");
 
-        sum = Arrays.stream(tokens)
+        return Arrays.stream(tokens)
                 .mapToInt(Integer::parseInt)
                 .sum();
-
-        System.out.println(sum);
     }
 }
