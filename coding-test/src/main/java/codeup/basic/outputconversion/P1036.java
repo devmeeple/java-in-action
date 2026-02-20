c4ad27ca8d003e37449d6ae06ad17401158cd51c
TaeGeun Jang <devmeeple@gmail.com>
2026-02-20 11:03:25 +0900

    [#15] P1036 영문자 1개 입력받아 10진수로 출력

---
+++ coding-test/src/main/java/codeup/basic/outputconversion/P1036.java
@@ -0,0 +1,16 @@
+package codeup.basic.outputconversion;
+
+import java.util.Scanner;
+
+public class P1036 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+        int result = input.charAt(0);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
