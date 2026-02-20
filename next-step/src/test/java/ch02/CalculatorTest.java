d2f5077db66c35aa6b0a809a431e96aa1b697a5b
TaeGeun Jang <devmeeple@gmail.com>
2026-02-20 14:46:58 +0900

    feat: @BeforeEach를 사용하여 인스턴스 초기화

--- next-step/src/test/java/ch02/CalculatorTest.java
+++ next-step/src/test/java/ch02/CalculatorTest.java
@@ -1,20 +1,26 @@
 package ch02;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator cal;
+
+    @BeforeEach
+    void setUp() {
+        cal = new Calculator();
+    }
+
     @Test
     void add() {
-        Calculator cal = new Calculator();
         assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     void subtract() {
-        Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6, 3));
     }
 
