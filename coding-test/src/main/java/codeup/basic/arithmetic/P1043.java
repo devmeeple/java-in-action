1cabf761c5118add0eb0482a2375ae4abeb719f7
TaeGeun Jang <devmeeple@gmail.com>
2026-02-21 19:42:34 +0900

    feat: P1043 정수 2개 입력받아 나눈 나머지 출력

---
+++ coding-test/src/main/java/codeup/basic/arithmetic/P1043.java
@@ -0,0 +1,17 @@
+package codeup.basic.arithmetic;
+
+import java.util.Scanner;
+
+public class P1043 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int result = a % b;
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
