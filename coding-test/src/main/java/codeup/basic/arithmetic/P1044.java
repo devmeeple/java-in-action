ff38fb695ecec65178e02c66f8be543e448b7d3e
TaeGeun Jang <devmeeple@gmail.com>
2026-02-21 19:44:33 +0900

    feat: P1044 정수 1개 입력받아 1 더해 출력

---
+++ coding-test/src/main/java/codeup/basic/arithmetic/P1044.java
@@ -0,0 +1,14 @@
+package codeup.basic.arithmetic;
+
+import java.util.Scanner;
+
+public class P1044 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long number = scanner.nextLong();
+        System.out.println(number + 1);
+
+        scanner.close();
+    }
+}
