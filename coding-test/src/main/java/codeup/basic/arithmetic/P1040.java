ec1ddff8c5aa71c3115ad5b1c75f72f706da9905
TaeGeun Jang <devmeeple@gmail.com>
2026-02-21 16:31:17 +0900

    feat: P1040 정수 1개 입력받아 부호 바꿔 출력

---
+++ coding-test/src/main/java/codeup/basic/arithmetic/P1040.java
@@ -0,0 +1,14 @@
+package codeup.basic.arithmetic;
+
+import java.util.Scanner;
+
+public class P1040 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        System.out.println(-number);
+
+        scanner.close();
+    }
+}
