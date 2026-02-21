358430b7258f134781550be72613df811ca828e5
TaeGeun Jang <devmeeple@gmail.com>
2026-02-21 19:38:04 +0900

    refactor: P1042 처음부터 문자열이 아닌 정수 2개를 입력받도록 개선

--- coding-test/src/main/java/codeup/basic/arithmetic/P1042.java
+++ coding-test/src/main/java/codeup/basic/arithmetic/P1042.java
@@ -1,19 +1,13 @@
 package codeup.basic.arithmetic;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1042 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String value = scanner.nextLine();
-        int[] numbers = Arrays.stream(value.split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        int a = numbers[0];
-        int b = numbers[1];
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
         int result = a / b;
         System.out.println(result);
