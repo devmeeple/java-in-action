610096d5b113c9b6305d9048755d65e0848c8823
장태근 <devmeeple@gmail.com>
2026-02-27 21:32:40 +0900

    refactor: P1064 조건문으로 가독성 개선

--- coding-test/src/main/java/codeup/basic/ternary/P1064.java
+++ coding-test/src/main/java/codeup/basic/ternary/P1064.java
@@ -10,8 +10,14 @@ public class P1064 {
         int number2 = scanner.nextInt();
         int number3 = scanner.nextInt();
 
-        int result = number1 < number2 ? number1 : number2;
-        result = result < number3 ? result : number3;
+        int result = number1;
+
+        if (number2 < result) {
+            result = number2;
+        }
+        if (number3 < result) {
+            result = number3;
+        }
 
         System.out.println(result);
         scanner.close();
