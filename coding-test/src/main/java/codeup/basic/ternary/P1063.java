0d349a0c4f81dfa2baacc937bb6b452b381e4079
장태근 <devmeeple@gmail.com>
2026-02-27 21:09:43 +0900

    feat: P1063 두 정수 입력받아 큰 수 출력

---
+++ coding-test/src/main/java/codeup/basic/ternary/P1063.java
@@ -0,0 +1,17 @@
+package codeup.basic.ternary;
+
+import java.util.Scanner;
+
+public class P1063 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        int result = number1 > number2 ? number1 : number2;
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
