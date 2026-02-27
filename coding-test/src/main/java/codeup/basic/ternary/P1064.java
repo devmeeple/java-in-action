0cce9753ea89fc87ad4b6af874035a1db05a2958
장태근 <devmeeple@gmail.com>
2026-02-27 21:30:52 +0900

    feat: P1064 정수 3개 입력받아 가장 작은 수 출력

---
+++ coding-test/src/main/java/codeup/basic/ternary/P1064.java
@@ -0,0 +1,19 @@
+package codeup.basic.ternary;
+
+import java.util.Scanner;
+
+public class P1064 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+
+        int result = number1 < number2 ? number1 : number2;
+        result = result < number3 ? result : number3;
+
+        System.out.println(result);
+        scanner.close();
+    }
+}
