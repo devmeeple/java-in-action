94904a6fdb3fcce5a50ccd0408ce85645d4e1a56
TaeGeun Jang <devmeeple@gmail.com>
2026-03-01 23:06:36 +0900

    refactor: P1069 grade로 변수명을 변경하여 의미 전달

--- coding-test/src/main/java/codeup/basic/condition/P1069.java
+++ coding-test/src/main/java/codeup/basic/condition/P1069.java
@@ -6,28 +6,18 @@ public class P1069 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String evaluate = scanner.next();
-        String evaluationContent = "";
-
+        String grade = scanner.next();
         // TODO: 문자열이 아닌 문자로 받을 때, default 문의 break은 생략 가능하다 2026-03-01
-        switch (evaluate) {
-            case "A":
-                evaluationContent = "best!!!";
-                break;
-            case "B":
-                evaluationContent = "good!!";
-                break;
-            case "C":
-                evaluationContent = "run!";
-                break;
-            case "D":
-                evaluationContent = "slowly~";
-                break;
-            default:
-                evaluationContent = "what?";
-                break;
-        }
-        System.out.println(evaluationContent);
+        // TODO: Switch Statements
+        String message = switch (grade) {
+            case "A" -> "best!!!";
+            case "B" -> "good!!";
+            case "C" -> "run!";
+            case "D" -> "slowly~";
+            default -> "what?";
+        };
+
+        System.out.println(message);
 
         scanner.close();
     }
