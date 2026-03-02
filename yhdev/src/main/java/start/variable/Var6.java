478e033fc46e2e8748d85fdc250923234ecc74d3
TaeGeun Jang <devmeeple@gmail.com>
2026-03-02 11:08:54 +0900

    feat: 변수 선언과 초기화

---
+++ yhdev/src/main/java/start/variable/Var6.java
@@ -0,0 +1,10 @@
+package start.variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+
+//        System.out.println(a); Compile error: Variable 'a' might not have been initialized
+    }
+}
