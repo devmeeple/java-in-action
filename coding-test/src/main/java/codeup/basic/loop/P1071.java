36ef0b233e728d4c4af41d563d0c01ba10a62920
TaeGeun Jang <devmeeple@gmail.com>
2026-03-03 23:07:48 +0900

    refactor(loop): P1071 while 조건에 sentinel value를 사용하도록 구조 개선

--- coding-test/src/main/java/codeup/basic/loop/P1071.java
+++ coding-test/src/main/java/codeup/basic/loop/P1071.java
@@ -7,13 +7,10 @@ public class P1071 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            int n = scanner.nextInt();
-
-            if (n == 0) {
-                break;
-            }
+        int n = scanner.nextInt();
+        while (n != 0) {
             System.out.println(n);
+            n = scanner.nextInt();
         }
 
         scanner.close();
