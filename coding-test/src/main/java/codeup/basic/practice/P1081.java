5220d36defabcd08ceec220a5e1e90fff2e8f60b
TaeGeun Jang <devmeeple@gmail.com>
2026-03-04 17:10:57 +0900

    feat(practice): P1081 주사위를 2개 던지면?(중첩 반복문)

---
+++ coding-test/src/main/java/codeup/basic/practice/P1081.java
@@ -0,0 +1,20 @@
+package codeup.basic.practice;
+
+import java.util.Scanner;
+
+public class P1081 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+
+        scanner.close();
+    }
+}
