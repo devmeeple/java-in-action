5e245a18b201648d5bfab33d1a862514408a67a6
TaeGeun Jang <devmeeple@gmail.com>
2026-03-04 23:07:00 +0900

    feat(practice): P1087 여기까지! 이제 그만~

---
+++ coding-test/src/main/java/codeup/basic/practice/P1087.java
@@ -0,0 +1,25 @@
+package codeup.basic.practice;
+
+import java.util.Scanner;
+
+public class P1087 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
+
+        while (true) {
+            sum += i;
+            if (sum >= number) {
+                break;
+            }
+            i++;
+        }
+        System.out.println(sum);
+
+        scanner.close();
+    }
+}
