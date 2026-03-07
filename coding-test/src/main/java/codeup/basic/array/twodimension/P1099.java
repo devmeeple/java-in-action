cd8878d5083f8e50c1cae22c61e15099be12ce51
TaeGeun Jang <devmeeple@gmail.com>
2026-03-07 12:39:34 +0900

    refactor(array): P1099 미로 입력, 이동, 출력 로직 메서드 분리

--- coding-test/src/main/java/codeup/basic/array/twodimension/P1099.java
+++ coding-test/src/main/java/codeup/basic/array/twodimension/P1099.java
@@ -8,36 +8,67 @@ public class P1099 {
     private static final int WALL = 1;
     private static final int FOOD = 2;
     private static final int PATH = 9;
+    private static final int START_ROW = 1;
+    private static final int START_COLUMN = 1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[][] maze = new int[MAZE_SIZE][MAZE_SIZE];
 
+        readMaze(scanner, maze);
+        moveAnt(maze);
+        printMaze(maze);
+
+        scanner.close();
+    }
+
+    private static void readMaze(Scanner scanner, int[][] maze) {
         for (int row = 0; row < maze.length; row++) {
             for (int column = 0; column < maze[row].length; column++) {
                 maze[row][column] = scanner.nextInt();
             }
         }
+    }
 
-        int antRow = 1;
-        int antColumn = 1;
+    private static void moveAnt(int[][] maze) {
+        int antRow = START_ROW;
+        int antColumn = START_COLUMN;
+
         while (true) {
-            if (maze[antRow][antColumn] == FOOD) {
-                maze[antRow][antColumn] = PATH;
+            if (isFood(maze, antRow, antColumn)) {
+                markPath(maze, antRow, antColumn);
                 break;
             }
-            maze[antRow][antColumn] = PATH;
-            if (maze[antRow][antColumn + 1] != WALL) {
+            markPath(maze, antRow, antColumn);
+            if (canMoveRight(maze, antRow, antColumn)) {
                 antColumn++;
                 continue;
             }
-            if (maze[antRow + 1][antColumn] != WALL) {
+            if (canMoveDown(maze, antRow, antColumn)) {
                 antRow++;
                 continue;
             }
             break;
         }
+    }
 
+    private static boolean isFood(int[][] maze, int row, int column) {
+        return maze[row][column] == FOOD;
+    }
+
+    private static void markPath(int[][] maze, int row, int column) {
+        maze[row][column] = PATH;
+    }
+
+    private static boolean canMoveRight(int[][] maze, int antRow, int antColumn) {
+        return maze[antRow][antColumn + 1] != WALL;
+    }
+
+    private static boolean canMoveDown(int[][] maze, int antRow, int antColumn) {
+        return maze[antRow + 1][antColumn] != WALL;
+    }
+
+    private static void printMaze(int[][] maze) {
         for (int row = 0; row < maze.length; row++) {
             for (int column = 0; column < maze[row].length; column++) {
                 System.out.print(maze[row][column]);
@@ -47,7 +78,5 @@ public class P1099 {
             }
             System.out.println();
         }
-
-        scanner.close();
     }
 }
