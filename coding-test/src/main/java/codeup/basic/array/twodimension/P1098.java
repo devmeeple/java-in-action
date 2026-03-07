e6aa713f3ff5112fd892c0bb98fee0dd9f19da88
TaeGeun Jang <devmeeple@gmail.com>
2026-03-07 12:11:38 +0900

    refactor(array): P1098 막대 배치 로직을 메서드로 분리

--- coding-test/src/main/java/codeup/basic/array/twodimension/P1098.java
+++ coding-test/src/main/java/codeup/basic/array/twodimension/P1098.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class P1098 {
 
+    private static final int HORIZONTAL = 0;
+    private static final int VERTICAL = 1;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,27 +22,44 @@ public class P1098 {
             int startRow = scanner.nextInt() - 1;
             int startColumn = scanner.nextInt() - 1;
 
-            if (direction == 0) {
-                for (int offset = 0; offset < stickLength; offset++) {
-                    board[startRow][startColumn + offset] = 1;
-                }
-            } else {
-                for (int offset = 0; offset < stickLength; offset++) {
-                    board[startRow + offset][startColumn] = 1;
-                }
-            }
+            placeStick(board, stickLength, direction, startRow, startColumn);
         }
 
+        printBoard(board);
+        scanner.close();
+    }
+
+    private static void placeStick(int[][] board, int stickLength, int direction, int startRow, int startColumn) {
+        if (direction == HORIZONTAL) {
+            placeHorizontalStick(board, stickLength, startRow, startColumn);
+            return;
+        }
+        if (direction == VERTICAL) {
+            placeVerticalStick(board, stickLength, startRow, startColumn);
+        }
+    }
+
+    private static void placeHorizontalStick(int[][] board, int stickLength, int startRow, int startColumn) {
+        for (int offset = 0; offset < stickLength; offset++) {
+            board[startRow][startColumn + offset] = 1;
+        }
+    }
+
+    private static void placeVerticalStick(int[][] board, int stickLength, int startRow, int startColumn) {
+        for (int offset = 0; offset < stickLength; offset++) {
+            board[startRow + offset][startColumn] = 1;
+        }
+    }
+
+    private static void printBoard(int[][] board) {
         for (int row = 0; row < board.length; row++) {
             for (int column = 0; column < board[row].length; column++) {
-                System.out.print(board[row][column]);
-                if (column != board[row].length - 1) {
+                if (column > 0) {
                     System.out.print(" ");
                 }
+                System.out.print(board[row][column]);
             }
             System.out.println();
         }
-
-        scanner.close();
     }
 }
