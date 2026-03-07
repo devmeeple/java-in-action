f58eaa7929723114058ea5f2a213ec793f80f0c7
TaeGeun Jang <devmeeple@gmail.com>
2026-03-07 11:38:21 +0900

    refactor(array): P1097 바둑판 뒤집기 로직을 메서드로 분리

--- coding-test/src/main/java/codeup/basic/array/twodimension/P1097.java
+++ coding-test/src/main/java/codeup/basic/array/twodimension/P1097.java
@@ -4,38 +4,59 @@ import java.util.Scanner;
 
 public class P1097 {
 
+    private static final int BOARD_SIZE = 19;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[][] board = new int[19][19];
+        int[][] board = new int[BOARD_SIZE][BOARD_SIZE];
 
-        for (int row = 0; row < board.length; row++) {
-            for (int column = 0; column < board[row].length; column++) {
-                board[row][column] = scanner.nextInt();
-            }
-        }
+        readBoard(board, scanner);
 
         int flipCount = scanner.nextInt();
         for (int i = 0; i < flipCount; i++) {
             int rowToFlip = scanner.nextInt() - 1;
             int columnToFlip = scanner.nextInt() - 1;
-
-            for (int column = 0; column < board.length; column++) {
-                if (board[rowToFlip][column] == 0) {
-                    board[rowToFlip][column] = 1;
-                } else {
-                    board[rowToFlip][column] = 0;
-                }
-            }
-
-            for (int row = 0; row < board.length; row++) {
-                if (board[row][columnToFlip] == 0) {
-                    board[row][columnToFlip] = 1;
-                } else {
-                    board[row][columnToFlip] = 0;
-                }
-            }
+            flipCross(board, rowToFlip, columnToFlip);
         }
 
+        printBoard(board);
+        scanner.close();
+    }
+
+    private static void readBoard(int[][] board, Scanner scanner) {
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board[row].length; column++) {
+                board[row][column] = scanner.nextInt();
+            }
+        }
+    }
+
+    private static void flipCross(int[][] board, int rowToFlip, int columnToFlip) {
+        flipRow(board, rowToFlip);
+        flipColumn(board, columnToFlip);
+    }
+
+    private static void flipRow(int[][] board, int rowToFlip) {
+        for (int column = 0; column < board.length; column++) {
+            if (board[rowToFlip][column] == 0) {
+                board[rowToFlip][column] = 1;
+            } else {
+                board[rowToFlip][column] = 0;
+            }
+        }
+    }
+
+    private static void flipColumn(int[][] board, int columnToFlip) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][columnToFlip] == 0) {
+                board[row][columnToFlip] = 1;
+            } else {
+                board[row][columnToFlip] = 0;
+            }
+        }
+    }
+
+    private static void printBoard(int[][] board) {
         for (int row = 0; row < board.length; row++) {
             for (int column = 0; column < board[row].length; column++) {
                 System.out.print(board[row][column]);
@@ -45,7 +66,5 @@ public class P1097 {
             }
             System.out.println();
         }
-
-        scanner.close();
     }
 }
