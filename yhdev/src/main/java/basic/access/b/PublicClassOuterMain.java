7f8b6c53120b9986014b01b91c41f4ca9de61886
TaeGeun Jang <devmeeple@gmail.com>
2026-03-11 13:37:27 +0900

    feat: 접근 제어자 사용 - 클래스 레벨

---
+++ yhdev/src/main/java/basic/access/b/PublicClassOuterMain.java
@@ -0,0 +1,15 @@
+package basic.access.b;
+
+// import basic.access.a.DefaultClass1;
+// import basic.access.a.DefaultClass2;
+import basic.access.a.PublicClass;
+
+public class PublicClassOuterMain {
+
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+
+//        DefaultClass1 class1 = new DefaultClass1(); Cannot resolve symbol 'DefaultClass1'
+//        DefaultClass2 class2 = new DefaultClass2(); Cannot resolve symbol 'DefaultClass2'
+    }
+}
