29de38d34bdb85d5edd5cb3fc84dd91486dfe9cd
TaeGeun Jang <devmeeple@gmail.com>
2026-03-16 15:16:31 +0900

    feat: 불변 객체 - 도입

---
+++ yhdev/src/main/java/mid1/lang/immutable/address/RefMain2.java
@@ -0,0 +1,17 @@
+package mid1.lang.immutable.address;
+
+public class RefMain2 {
+
+    public static void main(String[] args) {
+        ImmutableAddress a = new ImmutableAddress("서울");
+        ImmutableAddress b = a; // 참조값 대입을 막을 수 있는 방법이 없다.
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+//        b.setValue("부산"); // Cannot resolve method 'setValue' in 'ImmutableAddress'
+        b = new ImmutableAddress("부산");
+        System.out.println("부산 -> b");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
