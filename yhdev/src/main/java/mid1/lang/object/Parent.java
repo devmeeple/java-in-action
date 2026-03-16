e8f79d3b4a69ed187bcf59db0d0506df17fd4ea0
TaeGeun Jang <devmeeple@gmail.com>
2026-03-16 12:03:57 +0900

    feat: Object 클래스

---
+++ yhdev/src/main/java/mid1/lang/object/Parent.java
@@ -0,0 +1,9 @@
+package mid1.lang.object;
+
+// 부모가 없으면 묵식적으로 Object 클래스를 상속 받는다.
+public class Parent {
+
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}
