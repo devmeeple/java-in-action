8de7eb3342527ae116b67d4b9ebc84aa645b8c87
장태근 <devmeeple@gmail.com>
2026-03-17 21:44:22 +0900

    feat: String 클래스 문제와 풀이2 - (11) reverse()

---
+++ yhdev/src/main/java/mid1/lang/string/test/TestString11.java
@@ -0,0 +1,10 @@
+package mid1.lang.string.test;
+
+public class TestString11 {
+
+    public static void main(String[] args) {
+        String str = "Hello Java";
+        StringBuilder reversed = new StringBuilder(str).reverse();
+        System.out.println(reversed);
+    }
+}
