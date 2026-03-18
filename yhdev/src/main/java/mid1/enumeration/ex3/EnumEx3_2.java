c9c6981069f250211a6dfc59460103edda389f02
장태근 <devmeeple@gmail.com>
2026-03-18 18:10:20 +0900

    refactor: 열거형 - Enum Type
    - static import

---
+++ yhdev/src/main/java/mid1/enumeration/ex3/EnumEx3_2.java
@@ -0,0 +1,15 @@
+package mid1.enumeration.ex3;
+
+public class EnumEx3_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+
+/*
+        Grade myGrade = new Grade(); // Enum types cannot be instantiated
+        double result = discountService.discount(myGrade, price);
+        System.out.println("result price: " + result);
+*/
+    }
+}
