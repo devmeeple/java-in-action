ce21df8a09de2b528bc5c283b2d25f70f527df7c
장태근 <devmeeple@gmail.com>
2026-03-23 19:03:00 +0900

    refactor: 다형성을 통한 중복 해결 시도

---
+++ yhdev/src/main/java/mid2/generic/ex1/BoxMain2.java
@@ -0,0 +1,21 @@
+package mid2.generic.ex1;
+
+public class BoxMain2 {
+
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get(); // Object -> Integer 캐스팅
+        System.out.println("integer = " + integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("hello");
+        String str = (String) stringBox.get(); // Object -> String 캐스팅
+        System.out.println("str = " + str);
+
+        // 잘못된 타입의 인수 전달시
+        integerBox.set("문자100");
+        Integer result = (Integer) integerBox.get(); // String -> Integer 캐스팅 Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
+        System.out.println("result = " + result);
+    }
+}
