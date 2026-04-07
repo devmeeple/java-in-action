b69178ede7e2527a6094be3fd24381fa835bd8b1
장태근 <devmeeple@gmail.com>
2026-04-07 19:52:23 +0900

    build(ch1): AssertJ 의존성 추가

--- thinkdast/build.gradle.kts
+++ thinkdast/build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // AssertJ 테스트 코드 가독성, 편의성 라이브러리
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
